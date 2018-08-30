4927f79d1bef114f01b33678c6a86e285892d5d0
alia <alastair@heypres.to>
2018-08-30 21:36:44 +0100

    move to optional

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const expressSession = require('express-session');
 const cookieParser = require('cookie-parser');
 const CryptoJS = require("crypto-js");
 const fetch = require('node-fetch');
-const { 
+const {
 	startIntegration,
-newCode 
+	newCode
 } = require('./backend/unleashedGs')
 const { PORT } = require('./backend/configs/backend');
 const config = require('./backend/configs/backend');
-const { sendIndexHtml} = require('./backend/configs/indexHtml');
+const { sendIndexHtml } = require('./backend/configs/indexHtml');
 
 let app = express()
 
 
 
 app
-.get('*.js', (req, res, next) => {
-	req.url = req.url + '.gz';
-	res.set('Content-Encoding', 'gzip');
-	next();
-  	})
+	.get('*.js', (req, res, next) => {
+		req.url = req.url + '.gz';
+		res.set('Content-Encoding', 'gzip');
+		next();
+	})
 	.use('/build', express.static('build'))
 	.use(bodyParser.json())
 	.use(cookieParser())
-	.get('/get/refresh', startIntegration )
+	.get('/get/refresh', startIntegration)
 	.post('/post/newCode', newCode)
 	.get('/*', sendIndexHtml)
 	.listen(config.PORT, '0.0.0.0', () => {
